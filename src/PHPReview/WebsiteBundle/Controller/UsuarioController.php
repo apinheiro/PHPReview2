1743d0752a3425aa41c8be07626cd437a3310c8b
Andre Pinheiro <digital3i@gmail.com>
2012-07-05 16:31:02 -0300

    #9 - Criando sistema para reinserir nova senha

--- src/PHPReview/WebsiteBundle/Controller/UsuarioController.php
+++ src/PHPReview/WebsiteBundle/Controller/UsuarioController.php
@@ -8,6 +8,8 @@ use JMS\SecurityExtraBundle\Annotation\Secure;
 use PHPReview\WebsiteBundle\Form;
 use PHPReview\WebsiteBundle\Entity\Usuario as Usuario;
 use PHPReview\AdminBundle\Entity\Chave as Chave;
+use PHPReview\AdminBundle\Entity\TipoChave as TipoChave;
+
 
 class UsuarioController extends Controller
 {
@@ -19,6 +21,21 @@ class UsuarioController extends Controller
         return array('name' => $name);
     }
         
+    /**
+     * @Template()
+     * @return type 
+     */
+    public function getContagemAction(){
+        
+      $qb = $this->getDoctrine()->getEntityManager()->createQueryBuilder();
+      $qb->select('count(u.id)');
+      $qb->from('WebsiteBundle:Usuario','u');
+      $qb->where('u.in_ativo = 1');
+
+       $count = $qb->getQuery()->getSingleScalarResult();
+       return array('total'=>$count);
+    }
+    
     /**
      * @Template()
      */
@@ -40,18 +57,13 @@ class UsuarioController extends Controller
               $usuario->setDtAtualizacao(\DateTime::createFromFormat(\Datetime::ATOM, date(\Datetime::ATOM)));
               $usuario->setInAtivo(false);
 
-              $chave = new Chave();
-              $chave->setChave(Chave::generateChave());
-              $chave->setDataExpira(\DateTime::createFromFormat('d/m/Y H:i:s', date('d/m/Y H:i:s',strtotime("+3 days"))));
-              $chave->setDataRegistro(new \DateTime());
-              $chave->setInUsado(false);
-              $chave->setInExpirado(false);
-              $chave->setTipoChave($em->getRepository('AdminBundle:TipoChave')->find(1));
-              $chave->setUsuario($usuario);
+              $chave = $this->geraChave($usuario, $em);
 
               $em->persist($usuario);
               $em->persist($chave);
               $em->flush();
+              
+              $this->enviaEmailConfirmacao($chave);
               
               $this->get('session')->setFlash('notice', 'Usuario criado com sucesso!');
               return $this->redirect($this->generateUrl('homepage_cadastro_sucesso'));   
@@ -70,26 +82,74 @@ class UsuarioController extends Controller
     }
     
     /**
+     * Recuperar Senha
+     * 
+     * Método para o usuário informar o seu e-mail para recuperar a senha.
+     * 
      * @Template() 
      */
     public function recuperaSenhaAction(){
-        
+       $request = $this->getRequest();
+       
+       if ($request->getMethod() == 'POST'){
+           $email = $request->request->get('email');
+           
+           $em = $this->getDoctrine()->getEntityManager();
+           $usuario = $em->getRepository('WebsiteBundle:Usuario')->findOneByDsEmail($email);
+           
+           $chave = $this->geraChave($usuario, $em, TipoChave::CONFIRMAR_CADASTRO);
+           
+           $this->persist($chave);
+           $this->flush();
+           
+           // aqui envia o e-mail pro usuario.
+           $this->enviaEmailConfirmacao($chave);
+           
+           $this->get('session')->setFlash('notice','Foi enviado um e-mail com um código para você revalidar a sua senha.');
+           return $this->redirect($this->generateUrl('validar_chave_usuario2'));
+       }
+       return array();
     }
     
     /**
-     * @Template()
-     * @return type 
+     * @Template() 
      */
-    public function getContagemAction(){
-        
-      $qb = $this->getDoctrine()->getEntityManager()->createQueryBuilder();
-      $qb->select('count(u.id)');
-      $qb->from('WebsiteBundle:Usuario','u');
-      $qb->where('u.in_ativo = 1');
-
-       $count = $qb->getQuery()->getSingleScalarResult();
-       return array('total'=>$count);
-    }
+   public function novaSenhaAction($token = null){
+       // somente quem informar o token que poderá acessar esta página.
+       if (is_null($token)){
+           echo "TOKEN NULO";
+           # return $this->redirect($this->generateUrl('homepage'));
+       }
+       
+       $em = $this->getDoctrine()->getEntityManager();
+       $chave = $em->getRepository('AdminBundle:Chave')->findOneByChave($token);
+       
+       if (!$chave){
+           echo 'CHAVE NAO ENCONTRADA';
+          # return $this->redirect($this->generateUrl('homepage')); 
+       }
+       
+       if ($chave->getInExpirado() || $chave->getInUsado()){
+           return $this->redirect($this->generateUrl('validar_chave_usuario2'));
+       }
+       
+       $usuario = new Usuario();
+       $usuario = $chave->getUsuario();
+       
+       // Se o usuário submeteu o formulário, entao devemos processar a senha.
+       if ($this->getRequest()->getMethod() == 'POST' ){
+           $senha = $this->getRequest()->request->get('senha');
+           $usuario->setDsSenha($senha);
+           $usuario->criptografaSenha($this->get('security.encoder_factory'));
+           $usuario->setDtAtualizacao(\DateTime::createFromFormat(\Datetime::ATOM, date(\Datetime::ATOM)));
+           
+           $chave->setInUsado(true);
+           
+           $em->flush();
+           return $this->render('WebsiteBundle:Usuario:sucesso.html.twig',array('sucesso'=>1));
+       }
+       return array('usuario'=>$usuario,'token'=>$token);
+   }
     
     /**
      * Processa o token do usuário.
@@ -110,8 +170,6 @@ class UsuarioController extends Controller
              return array('sucesso'=>0);   
           }
         }
-        
-        $valido = false;
         
         $em = $this->getDoctrine()->getEntityManager();
         $chave = $em->getRepository('AdminBundle:Chave')->findOneByChave($_token);
@@ -137,6 +195,12 @@ class UsuarioController extends Controller
         }
 
         // Habilitando o usuário
+        if ($chave->getTipoChave()->getId() == TipoChave::NOVA_SENHA){
+            return $this->redirect(
+                       $this->generateUrl('recuperar_senha_usuario_form',
+                                          array('token'=>$_token)));
+        }
+        
         $usuario = $em->getRepository('WebsiteBundle:Usuario')->find($chave->getUsuario()->getId());
         $usuario->setInAtivo(true);
         $chave->setInUsado(true);
@@ -144,4 +208,26 @@ class UsuarioController extends Controller
         
         return array('sucesso'=>1);
     }
+    
+    private function geraChave($usuario, $em, $tipo_chave ){
+        $chave = new Chave();
+        $chave->setChave(Chave::generateChave());
+        $chave->setDataExpira(\DateTime::createFromFormat('d/m/Y H:i:s', date('d/m/Y H:i:s',strtotime("+3 days"))));
+        $chave->setDataRegistro(new \DateTime());
+        $chave->setInUsado(false);
+        $chave->setInExpirado(false);
+        $chave->setTipoChave($em->getRepository('AdminBundle:TipoChave')->find($tipo_chave));
+        $chave->setUsuario($usuario);
+        
+        return $chave;
+    }
+    
+    private function enviaEmailConfirmacao($chave){
+       $email = \Swift_Message::newInstance()
+               ->setSubject('Confirmação de cadastro')
+               ->setFrom('contato@phpreview.br')
+               ->setTo($usuario->getDsEmail())
+               ->setBody($this->renderView('WebsiteBundle:Emails:confirm_cadastro.html.twig',array('chave'=>$chave)));
+       $this->get('mailer')->send($email);
+   }
 }
